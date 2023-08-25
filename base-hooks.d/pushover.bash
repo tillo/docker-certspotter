8a13d96246fae6d21898df313ea0b85dd2f0374c
Martino Dell'Ambrogio <tillo@tillo.ch>
2023-08-25 15:05:28 +0200

    Modified to work in continuous mode with hook scripts

--- base-hooks.d/pushover.bash
+++ base-hooks.d/pushover.bash
@@ -3,12 +3,16 @@
 # shellcheck disable=SC1091
 source /certspotter/utils.bash
 
-if [ -n "${1}" ] && [ -n "${CS_PUSHOVER_TOKEN}" ] && [ -n "${CS_PUSHOVER_USER}" ]; then
+if [ -n "${1}" ]; then
+	SUMMARY=$1
+fi
+
+if [ -n "${SUMMARY}" ] && [ -n "${CS_PUSHOVER_TOKEN}" ] && [ -n "${CS_PUSHOVER_USER}" ]; then
 
   # From https://github.com/akusei/pushover-bash/blob/main/pushover.sh
   curl -X POST --silent \
     -H "Content-Type: application/json" \
-    -d "{\"token\":\"${CS_PUSHOVER_TOKEN}\",\"user\":\"${CS_PUSHOVER_USER}\",\"message\":\"$1\"}" \
+    -d "{\"token\":\"${CS_PUSHOVER_TOKEN}\",\"user\":\"${CS_PUSHOVER_USER}\",\"message\":\"$SUMMARY\"}" \
     "https://api.pushover.net/1/messages.json"
 
 fi
